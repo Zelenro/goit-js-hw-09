4793dc56f041988104191cb6f97d8d85f9aa9f76
Zelenro <125198729+Zelenro@users.noreply.github.com>
2023-07-06 22:22:50 +0300

    first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
+
+btnStart.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    btnStart.disabled = true;
+    body.style.backgroundColor = getRandomHexColor();
+  }, 500);
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(intervalId);
+  btnStart.disabled = false;
+  body.style.backgroundColor = '';
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,6 @@
+// import flatpickr from 'flatpickr';
+
+// import 'flatpickr/dist/flatpickr.min.css';
+
+// const input = document.querySelector('#datetime-picker');
+// console.log(input);
