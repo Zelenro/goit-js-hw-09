f501e2b9488ec3b0c4c99928287a7b8e03a5c703
Zelenro <125198729+Zelenro@users.noreply.github.com>
2023-07-09 12:38:45 +0300

    fix location files

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,6 +2,8 @@ const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 
+let intervalId = '';
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
