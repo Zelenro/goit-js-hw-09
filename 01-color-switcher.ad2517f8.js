d0fafddd256833d5e8f2fbee99d702e43909ffe6
Zelenro <125198729+Zelenro@users.noreply.github.com>
2023-07-09 09:39:31 +0000

    Deploying to gh-pages from @ Zelenro/goit-js-hw-09@f501e2b9488ec3b0c4c99928287a7b8e03a5c703 🚀

--- 01-color-switcher.ad2517f8.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){intervalId=setInterval((function(){t.disabled=!0,n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),500)})),e.addEventListener("click",(function(){clearInterval(intervalId),t.disabled=!1,n.style.backgroundColor=""}))}();
-//# sourceMappingURL=01-color-switcher.ad2517f8.js.map
